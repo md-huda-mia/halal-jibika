cbb0ed102bc8da5f1b547dc32c03273218c9ebfb
md-huda-mia <mdhudamia.dev@gmail.com>
2024-01-02 11:23:28 +0600

    navbar error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import CustomHooks from "../../Hooks/CustomHooks";
+import { FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { user, logout } = CustomHooks();
-  console.log(user);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-  return (
-    <div className={`${styles.navbar_section}  section_padding`}>
-      <div className={`${styles.navbar_area}`}>
-        {/* ==== Logo ===== */}
 
+  return (
+    <div className={`${styles.navbar_section} section_padding`}>
+      <div className={`${styles.navbar_area}`}>
         <div className={`${styles.logo}`}>
-          <button className={`${styles.menu_button}`} onClick={toggleMenu}>
-            <FaBars className={`${styles.bar_icon}`} />
+          {/* Use the onClick event for the toggle button */}
+          <button
+            className={`${styles.bar_icon} ${styles.menu_button}`}
+            onClick={toggleMenu}>
+            <FaBars />
           </button>
-          {/* <img
-            className="logoImg"
-            src="https://i.ibb.co/P6MZ8FM/Screenshot-2023-12-31-002754-removebg-preview.png"
-            alt=""
-          /> */}
+
+          {/* ============ */}
+
           <h3 className={`${styles.logo_text}`}>
             Halal<span className={`${styles.logo_Span}`}>Jibika</span>{" "}
           </h3>
         </div>
-        {/* ==== navbar ====== */}
 
+        {/* Use a conditional class to show/hide the mobile menu */}
         <ul
           className={`${styles.navbar_menu} ${
             menuOpen ? styles.show_menu : ""
           }`}>
           <li>
-            <NavLink to="/" className={`${styles.nav_link} `}>
+            <NavLink to="/" className={`${styles.nav_link}`}>
               Home
             </NavLink>
           </li>
@@ -62,7 +62,6 @@ const Navbar = () => {
             </NavLink>
           </li>
         </ul>
-        {/* ===== user menu ==== */}
 
         <div className={`${styles.user_menu}`}>
           {user?.email ? (
